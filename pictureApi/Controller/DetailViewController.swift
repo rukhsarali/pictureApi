2aab517a3c0c990066ee89eff95cb8d9f988b8d0
Rukhsarali <rukhsaralichanna7@gmail.com>
2020-09-07 13:48:02 +0500

    new upadte two

--- pictureApi/Controller/DetailViewController.swift
+++ pictureApi/Controller/DetailViewController.swift
@@ -19,6 +19,7 @@ class DetailViewController: UIViewController {
         self.scrollView.maximumZoomScale = 6
         scrollView.delegate = self
         imageView.load(url: imageViewDetail!.url)
+        scrollView.backgroundColor = UIColor.black
     }
 }
 //MARK: - scrollView method
@@ -26,5 +27,21 @@ extension DetailViewController: UIScrollViewDelegate{
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
-    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if let image = imageView.image {
+            let ratioW = imageView.frame.width / image.size.width
+            let ratioH = imageView.frame.width / image.size.width
+            let ratio = ratioW < ratioH ? ratioW : ratioH
+            let newWidth = image.size.width * ratio
+            let newHeight = image.size.height * ratio
+            let conditionLeft = newWidth*scrollView.zoomScale > imageView.frame.width
+            let left = 0.5 * (conditionLeft ? newWidth - imageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+            let conditionTop = newHeight*scrollView.zoomScale > imageView.frame.height
+            let top = 0.5 * (conditionTop ? newHeight - imageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+            scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+        }
+        else{
+            scrollView.contentInset = .zero
+        }
+    }
 }
